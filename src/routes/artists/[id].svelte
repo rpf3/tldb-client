2f3a9b6f01d113ba90ebb099f2e01cf3a8f9cc23
Dickie Field <me@rpf3.io>
2021-04-07 10:33:16 -0400

    feat: Create an artist landing page
    This commit creates a new artist landing page route and links to it from
    the tracklist list component.

---
+++ src/routes/artists/[id].svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+	export async function load({ page, fetch }) {
+		const artistId = page.params.id;
+		const artist = await fetch(`http://localhost:5000/artists/${artistId}`).then((r) => r.json());
+		return {
+			props: {
+				artist: artist
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let artist: Artist;
+</script>
+
+<h2>{artist.name}</h2>
