4cc63aeb667ab84df11241d7ceae509bfdecea79
Dickie Field <me@rpf3.io>
2021-04-07 11:20:06 -0400

    feat: Display the full tracklist data
    This commit adds to the tracklist landing page to display the full
    tracklist information.

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,8 +3,26 @@ type Artist = {
 	name: string;
 };
 
+type Remix = {
+	name: string;
+	artist: Artist;
+};
+
+type Track = {
+	id: string;
+	name: string;
+	artist: Artist;
+	remix: Remix;
+};
+
+type IndexedTrack = {
+	index: number;
+	track: Track;
+};
+
 type Tracklist = {
 	id: string;
 	name: string;
 	artists: Artist[];
+	tracks: IndexedTrack[];
 };
