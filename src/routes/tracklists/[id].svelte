b36063be41bee970ce1ef69554fec90d19ed934d
Dickie Field <me@rpf3.io>
2021-04-07 11:20:06 -0400

    feat: Display the full tracklist data
    This commit adds to the tracklist landing page to display the full
    tracklist information.

--- src/routes/tracklists/[id].svelte
+++ src/routes/tracklists/[id].svelte
@@ -10,10 +10,39 @@
 			}
 		};
 	}
+
+	const getTrackDisplayName = function (track: Track) {
+		let displayName = track.name;
+		if (track.remix) {
+			displayName = `${displayName} (${track.remix.artist.name} ${track.remix.name})`;
+		}
+		return displayName;
+	};
 </script>
 
 <script lang="ts">
 	export let tracklist: Tracklist;
 </script>
 
-<h2>{tracklist.name}</h2>
+<div class="flex flex-col text-center mb-10">
+	<h1>{tracklist.name}</h1>
+</div>
+
+<ol class="list-none">
+	{#each tracklist.tracks as track, i}
+		<li class="flex items-center pb-4 mb-4 {i < tracklist.tracks.length - 1 ? 'border-b' : ''}">
+			<div>
+				<span>{track.index}</span>
+			</div>
+
+			<div class="ml-4">
+				<h2>{getTrackDisplayName(track.track)}</h2>
+				<p class="mt-2">
+					<a href="/artists/{track.track.artist.id}" class="cursor-pointer text-indigo-500"
+						>{track.track.artist.name}</a
+					>
+				</p>
+			</div>
+		</li>
+	{/each}
+</ol>
