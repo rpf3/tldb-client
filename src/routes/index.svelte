920e447c1c03c190cf4f99b24a19a7c33edc4d7b
Dickie Field <me@rpf3.io>
2021-04-07 14:52:18 -0400

    ref: Open the tracklist landing page in a new tab
    This commit changes the tracklist landing page link to use an external
    link svg as well as open in a new tab.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
 
 <ol class="list-none">
 	{#each tracklists as tracklist, i}
-		<li class="flex flex-row pb-4 mb-4 {i < tracklists.length - 1 ? 'border-b' : ''}">
+		<li class="flex items-center pb-4 mb-4 {i < tracklists.length - 1 ? 'border-b' : ''}">
 			<div class="flex-grow">
 				<span>{tracklist.name}</span>
 
@@ -33,9 +33,10 @@
 				</p>
 			</div>
 
-			<div class="h-8 w-8 rounded-full text-blue-500">
+			<div class="text-blue-500">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
 					fill="none"
 					viewBox="0 0 24 24"
 					stroke="currentColor"
@@ -44,13 +45,14 @@
 						stroke-linecap="round"
 						stroke-linejoin="round"
 						stroke-width="2"
-						d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+						d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
 					/>
-					<a href="/tracklists/{tracklist.id}">
-						<circle
-							cx="50%"
-							cy="50%"
-							r="50%"
+					<a href="/tracklists/{tracklist.id}" target="_blank">
+						<rect
+							x="0"
+							y="0"
+							width="100%"
+							height="100%"
 							class="fill-current"
 							fill-opacity="0"
 							stroke-opacity="0"
