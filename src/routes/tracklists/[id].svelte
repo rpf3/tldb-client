ad5003e18c7c1822fa71b5cc4787ff21b81b35e7
Dickie Field <me@rpf3.io>
2021-04-07 10:57:01 -0400

    feat: Create a tracklist landing page
    This commit creates a new tracklist landing page route.  It also adds a
    new svg to the tracklist list component which serves as a link into the
    tracklist landing page.

---
+++ src/routes/tracklists/[id].svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	export async function load({ page, fetch }) {
+		const tracklistId = page.params.id;
+		const tracklist = await fetch(
+			`http://localhost:5000/tracklists/${tracklistId}?verbose=1`
+		).then((r) => r.json());
+		return {
+			props: {
+				tracklist: tracklist
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let tracklist: Tracklist;
+</script>
+
+<h2>{tracklist.name}</h2>
