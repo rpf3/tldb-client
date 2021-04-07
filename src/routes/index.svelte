d96f8f4fcabd8e1aee5963aa8576ed1954aae52a
Dickie Field <me@rpf3.io>
2021-04-07 09:48:53 -0400

    feat: Create a TracklistList component
    This commit creates a new component that will display the names of a set
    of tracklists.  It also adds code to the main index page to fetch the
    top ten tracklists and pass them into this new component.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,53 +1,19 @@
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+<script lang="ts" context="module">
+	export async function load({ fetch }) {
+		const tracklists = await fetch('http://localhost:5000/tracklists?verbose=1').then((r) =>
+			r.json()
+		);
+		return {
+			props: {
+				tracklists: tracklists
+			}
+		};
+	}
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<script lang="ts">
+	import TracklistList from '$lib/TracklistList.svelte';
+	export let tracklists: Tracklist[];
+</script>
 
-	<Counter />
-
-	<p>
-		Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to
-		build Svelte apps.
-	</p>
-</main>
-
-<style style lang="postcss">
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		@apply text-center;
-		@apply p-4;
-		@apply mx-auto;
-	}
-
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		@apply text-6xl;
-		@apply font-thin;
-		@apply leading-tight;
-		@apply my-16 mx-auto;
-		@apply max-w-xs;
-	}
-
-	p {
-		@apply max-w-xs;
-		@apply my-8 mx-auto;
-		@apply leading-snug;
-	}
-
-	@screen sm {
-		h1 {
-			@apply max-w-none;
-		}
-
-		p {
-			@apply max-w-none;
-		}
-	}
-</style>
+<TracklistList {tracklists} />
