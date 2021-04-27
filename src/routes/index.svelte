c4598a4a475df3af6bcd3870eb90717529071e34
Dickie Field <me@rpf3.io>
2021-04-26 21:25:37 -0400

    feat: Add an IconLink component
    This commit adds a new component that encapsulates the icon link HTML
    and SVG so that we don't have to repeat it everywhere.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,6 +17,8 @@
 </script>
 
 <script lang="ts">
+	import IconLink from '$lib/IconLink.svelte';
+
 	export let pageNumber: number;
 	export let tracklists: Tracklist[];
 </script>
@@ -39,33 +41,7 @@
 				</p>
 			</div>
 
-			<div class="text-blue-500">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-					/>
-					<a href="/tracklists/{tracklist.id}" target="_blank">
-						<rect
-							x="0"
-							y="0"
-							width="100%"
-							height="100%"
-							class="fill-current"
-							fill-opacity="0"
-							stroke-opacity="0"
-						/>
-					</a>
-				</svg>
-			</div>
+			<IconLink href="/tracklists/{tracklist.id}" />
 		</li>
 	{/each}
 </ol>
