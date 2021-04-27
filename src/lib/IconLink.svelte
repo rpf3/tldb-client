bf72de375652f8f34e0c9e0a7704f51f7f53cb2a
Dickie Field <me@rpf3.io>
2021-04-26 21:25:37 -0400

    feat: Add an IconLink component
    This commit adds a new component that encapsulates the icon link HTML
    and SVG so that we don't have to repeat it everywhere.

---
+++ src/lib/IconLink.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<div class="text-blue-500">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-6 w-6"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+		/>
+		<a {href} target="_blank">
+			<rect
+				x="0"
+				y="0"
+				width="100%"
+				height="100%"
+				class="fill-current"
+				fill-opacity="0"
+				stroke-opacity="0"
+			/>
+		</a>
+	</svg>
+</div>
